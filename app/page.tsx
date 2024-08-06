fdcb650ab810f7b59ac7bfb03796b7552f428b86
Joy <jungheeyoo.joy@gmail.com>
2024-08-06 18:56:36 +0900

    Route Handlers

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import FormButton from '@/components/form-btn';
 import FormInput from '@/components/form-input';
 
 export default function Login() {
+  async function handleForm(formData: FormData) {
+    console.log(formData.get('email'), formData.get('password'));
+    console.log('i run in the server baby!');
+  }
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col items-center gap-2 *:font-medium">
