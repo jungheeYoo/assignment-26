04041497c6ae57daec64ae3e90df49a968533058
Joy <jungheeyoo.joy@gmail.com>
2024-08-08 01:54:13 +0900

    assignment-27, 에러시 박스 css 추가

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -17,13 +17,14 @@ export default function FormInput({
   name,
   icon,
 }: FormInputProps) {
+  const errorClass = errors.length > 0 ? 'border-red-400 ring-red-400' : '';
   return (
     <div className="flex flex-col relative">
       <div className="relative flex items-center">
         {icon && <div className="absolute left-5">{icon}</div>}
         <input
           name={name}
-          className={`bg-transparent rounded-full w-full h-12 p-4 focus:outline-none ring-2 focus:ring-2 transition ring-neutral-200 focus:ring-gray-400 border-none placeholder:text-neutral-400 ${
+          className={`bg-transparent rounded-full w-full h-12 p-4 focus:outline-none ring-2 focus:ring-2 transition ring-neutral-200 focus:ring-gray-400 border-none placeholder:text-neutral-400 ${errorClass} ${
             icon ? 'pl-12' : ''
           }`}
           type={type}
