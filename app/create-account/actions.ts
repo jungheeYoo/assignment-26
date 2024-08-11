c9eb5c8634b7303e0f49e1af14174493d197e8a6
Joy <jungheeyoo.joy@gmail.com>
2024-08-12 02:25:24 +0900

    assignment-29, 비밀번호 해싱 수정

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -56,7 +56,8 @@ const formSchema = z
       .toLowerCase()
       .refine((val) => val.endsWith('@zod.com'), {
         message: 'Only @zod.com emails are allowed',
-      }),
+      })
+      .refine(checkUniqueUsername, 'This username is already taken'),
     password: z
       .string({
         required_error: 'Password is required',
@@ -78,13 +79,15 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get('confirm_password'),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
+
   if (!result.success) {
     console.log(result.error.flatten());
-
     return result.error.flatten();
   } else {
     const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    console.log(hashedPassword);
+
     const user = await db.user.create({
       data: {
         username: result.data.username,
